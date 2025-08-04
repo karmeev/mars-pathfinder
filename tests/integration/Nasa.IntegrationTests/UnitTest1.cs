71d06dd5ddd839c952b87d02765bd98dccab13f2
karmeev <karmeevtech@gmail.com>
2025-08-04 16:30:13 +0300

    TEST(api): add project for integration tests

---
+++ tests/integration/Nasa.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Nasa.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
